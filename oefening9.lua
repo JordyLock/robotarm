c776e6ccabbd5b6917bd4fc2528c22f80524b7d6
DESKTOP-9RO9TVF\jordy <99039425@mydavinci.nl>
2016-10-04 15:26:13 +0200

    versie 1.2

--- oefening9.lua
+++ oefening9.lua
@@ -1,17 +1,18 @@
 require 'robot_arm'
-robot_arm:load_level('exercise 9')
+robot_arm:load_level('exercise 10')
 robot_arm.speed= 0.97
 
- for MoveRows = 1, 4 do
+ for MoveRows = 1, 8 do
  for Blocks = 1, MoveRows do
  robot_arm:grab()
- for MoveBlock = 1, 5 do
+ for MoveBlock = 1, 9 do
  robot_arm:move_right()
  end
  robot_arm:drop()
- for MoveBlack = 1, 5 do
+ for MoveBlack = 1, 9 do
  robot_arm:move_left()
  end
  end
  robot_arm:move_right()  
 end
+
